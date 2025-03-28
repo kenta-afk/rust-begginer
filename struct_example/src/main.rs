a3bc9407ee1af0e5682559c3d05d768e03c3cd63
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-28 13:12:27 +0900

    struct

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,6 +1,25 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+//特定の構造体に対してメソッドを定義する
+impl Rectangle {
+    //正方形の構造体を返す。SelfはRectangleを指す
+    fn square(width: u32) -> Self {
+        Self {
+            width,
+            height: width,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
 }
 
 fn main() {
@@ -12,17 +31,30 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
+
+    let square = Rectangle::square(60);
 
     rect.width = 60;
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
+
+    println!(
+        "The area of the square is {} square pixels.",
+        square.area()
+    );
+
+    
+
+    println!("rect :{:?}", rect);
+    println!("square: {:?}", square);
+
+    rect.set_width(100);
+
+    println!("rect :{:?}", rect);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
