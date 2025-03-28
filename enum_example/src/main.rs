7d778e6b8af901a2a70944587c168885b9d7c9aa
rglww8wsQI <kentarou9.60@icloud.com>
2025-03-28 13:26:04 +0900

    enum

--- enum_example/src/main.rs
+++ enum_example/src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+enum Message {
+        Quit,
+        Move {x: i32, y: i32},
+        Write(String),
+        ChangeColor(i32, i32, i32),
+}
+
+//enumでもimplを使える
+impl Message {
+    fn show_this_message(&self) {
+        println!("{:?}", self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut message = Message::Quit;
+    message.show_this_message();
+    //println!("{:?}", message);
+    message = Message::Move {x: 30, y: 40};
+    message.show_this_message();
+    //println!("{:?}", message);
+    message = Message::Write("rrrrrrust".to_string());
+    message.show_this_message();
+    //println!("{:?}", message);
+    message = Message::ChangeColor(255, 0, 0);
+    message.show_this_message();
+    //println!("{:?}", message);
 }
